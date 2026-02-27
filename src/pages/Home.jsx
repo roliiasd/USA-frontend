e7aa2e736452d003b39c3265da98e21987f64f24
Nimrod-v1 <meszaros.nimrod.383@dszcbaross.edu.hu>
2026-02-27 11:20:41 +0100

    hulye hoki style import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Navbar from "../components/Navbar";
-import "/src/styles/Home.css";
+import "../styles/Home.css";
 export default function Home() {
   return (
     <>
       <nav className="navbar navbar-expand-lg">
         <div className="navbarContainer">
-          <Navbar homePage={"/"} FAQ={"/"} aboutUs={"/"} />
+          <Navbar homePage={"/"} FAQ={"/"} aboutUs={"/"}/>
+          
         </div>
       </nav>
     </>
